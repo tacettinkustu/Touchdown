c519852bd2959b09b521fc7990f66140afeca9a9
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-17 21:30:02 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Tacettin Küstü on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

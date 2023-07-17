d667baf395fd1f2fa3eab6b6fc888c6283ee8fc5
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-17 22:10:49 +0300

    Implement footerView component into contentView file

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -9,18 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        FooterView()
+            .padding(.horizontal)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 14")
     }
 }

1d9fde918f68fcf3bf6875849bd4fbe401ade1b3
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-17 22:01:20 +0300

    Create constants file to manage our constants

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Tacettin Küstü on 17.07.2023.
+//
+
+import SwiftUI
+
+// DATA
+// COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC

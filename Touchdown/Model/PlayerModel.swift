c9b3eb6f1d1bd8bfca0c23c0bc8650a38ed3a777
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-18 20:21:07 +0300

    Create a new model for player data

---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Tacettin Küstü on 18.07.2023.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+  let id: Int
+  let image: String
+}

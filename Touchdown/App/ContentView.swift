1846b73f1063796a04cb3ab8265ae383cd7fc4b7
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-18 20:43:11 +0300

    Place featuredTabView into contentView

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -18,10 +18,17 @@ struct ContentView: View {
                     .background(Color.white)
                     .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
                 
-                Spacer()
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    VStack(spacing: 0) {
+                        FeaturedTabView()
+                            .frame(height: UIScreen.main.bounds.width / 1.475) // This will fix the layout rendering priority issue by using the screen's aspect ratio.
+                            .padding(.vertical, 20)
+                        
+                        FooterView()
+                            .padding(.horizontal)
+                    } //: VSTACK
+                }) //: SCROLL
                 
-                FooterView()
-                    .padding(.horizontal)
             }
             .background(colorBackground.ignoresSafeArea(.all, edges: .all))
         }

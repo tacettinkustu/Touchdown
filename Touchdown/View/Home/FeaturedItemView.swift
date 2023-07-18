d87b80b8296412954edd348c99fe3faacf6d3538
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-18 20:34:17 +0300

    Build a new featuredItemView for a single item

---
+++ Touchdown/View/Home/FeaturedItemView.swift
@@ -0,0 +1,30 @@
+//
+//  FeaturedItemView.swift
+//  Touchdown
+//
+//  Created by Tacettin Küstü on 18.07.2023.
+//
+
+import SwiftUI
+
+struct FeaturedItemView: View {
+    
+    let player: Player
+    
+    var body: some View {
+        Image(player.image)
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(12)
+    }
+}
+
+struct FeaturedItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedItemView(player: players[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(colorBackground)
+    }
+}
+

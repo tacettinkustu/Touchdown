2ba70865938c58dab6c8ef1215d695d3faa7d41d
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-18 17:31:14 +0300

    Build a new logoView to display on nabber

---
+++ Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,37 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Tacettin Küstü on 18.07.2023.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+  var body: some View {
+    HStack(spacing: 4) {
+      Text("Touch".uppercased())
+        .font(.title3)
+        .fontWeight(.black)
+        .foregroundColor(.black)
+      
+      Image("logo-dark")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 30, height: 30, alignment: .center)
+      
+      Text("Down".uppercased())
+        .font(.title3)
+        .fontWeight(.black)
+        .foregroundColor(.black)
+    } //: HSTACK
+  }
+}
+
+struct LogoView_Previews: PreviewProvider {
+  static var previews: some View {
+    LogoView()
+      .previewLayout(.sizeThatFits)
+      .padding()
+  }
+}

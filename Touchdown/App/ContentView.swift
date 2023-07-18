f5ae48214f554a4d962a0c8b1c8c28e40951cf4b
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-18 20:17:03 +0300

    Implement all new views into contentView component

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        FooterView()
-            .padding(.horizontal)
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+            }
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        }
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
 

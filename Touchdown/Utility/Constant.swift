7fac56fe74afc95305ff9d1a94f15209392115ff
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-19 17:36:22 +0300

    Build a new CategoryItemView component

--- Touchdown/Utility/Constant.swift
+++ Touchdown/Utility/Constant.swift
@@ -10,6 +10,7 @@ import SwiftUI
 // DATA
 
 let players: [Player] = Bundle.main.decode("player.json")
+let categories: [Category] = Bundle.main.decode("category.json")
 
 // COLOR
 
@@ -17,6 +18,13 @@ let colorBackground: Color = Color("ColorBackground")
 let colorGray: Color = Color(UIColor.systemGray4)
 
 // LAYOUT
+
+let columnSpacing: CGFloat = 10
+let rowSpacing: CGFloat = 10
+var gridLayout: [GridItem] {
+  return Array(repeating: GridItem(.flexible(), spacing: rowSpacing), count: 2)
+}
+
 // UX
 // API
 // IMAGE

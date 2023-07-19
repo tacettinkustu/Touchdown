7e9cd436efc414c1a1fb393aad8ce7e64a2feae8
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-19 21:35:58 +0300

    Create a new model for brand data

---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Tacettin Küstü on 19.07.2023.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}

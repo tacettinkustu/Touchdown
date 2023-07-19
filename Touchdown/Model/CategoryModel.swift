b6011d036ae35e189be23e60fa98197dfef63697
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-19 17:27:42 +0300

    Create a model for category data

---
+++ Touchdown/Model/CategoryModel.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryModel.swift
+//  Touchdown
+//
+//  Created by Tacettin Küstü on 19.07.2023.
+//
+
+import Foundation
+
+struct Category: Codable, Identifiable {
+  let id: Int
+  let name: String
+  let image: String
+}

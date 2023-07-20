810da686ba432e625041d3f7e2dab1652f83f9bd
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-20 18:33:42 +0300

    Create an empty ProductDetailView layout

---
+++ Touchdown/App/ProductDetailView.swift
@@ -0,0 +1,38 @@
+//
+//  ProductDetailView.swift
+//  Touchdown
+//
+//  Created by Tacettin Küstü on 20.07.2023.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+            
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5, content: {
+            // NAVBAR
+            
+            Text(sampleProduct.name)
+            
+            Spacer()
+
+        }) //: VSTACK
+        .ignoresSafeArea(.all, edges: .all)
+        .background(
+          Color(
+            red: sampleProduct.red,
+            green: sampleProduct.green,
+            blue: sampleProduct.blue
+          ).ignoresSafeArea(.all, edges: .all)
+        )
+    }
+}
+
+struct ProductDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailView()
+            .previewLayout(.fixed(width: 375, height: 812))
+    }
+}
+

1a7ee41276a0730b008a3a0e9aa640c192736c2a
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-21 22:57:26 +0300

    Create a customShape utility and place it

--- Touchdown/App/ProductDetailView.swift
+++ Touchdown/App/ProductDetailView.swift
@@ -8,30 +8,45 @@
 import SwiftUI
 
 struct ProductDetailView: View {
-            
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 5, content: {
             // NAVBAR
             NavigationBarDetailView()
-              .padding(.horizontal)
-              .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                .padding(.horizontal)
+                .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
             
             HeaderDetailView()
-              .padding(.horizontal)
+                .padding(.horizontal)
             
             TopPartDetailView()
                 .padding(.horizontal)
-                        
+            
+            VStack(alignment: .center, spacing: 0, content: {
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    Text(sampleProduct.description)
+                        .font(.system(.body, design: .rounded))
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.leading)
+                }) //: SCROLL
+            }) //: VSTACK
+            .padding(.horizontal)
+            .background(
+                Color.white
+                    .clipShape(CustomShape())
+                    .padding(.top, -105)
+            )
+            
             Spacer()
-
+            
         }) //: VSTACK
         .ignoresSafeArea(.all, edges: .all)
         .background(
-          Color(
-            red: sampleProduct.red,
-            green: sampleProduct.green,
-            blue: sampleProduct.blue
-          ).ignoresSafeArea(.all, edges: .all)
+            Color(
+                red: sampleProduct.red,
+                green: sampleProduct.green,
+                blue: sampleProduct.blue
+            ).ignoresSafeArea(.all, edges: .all)
         )
     }
 }

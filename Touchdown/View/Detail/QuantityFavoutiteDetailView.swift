d1cc099312b259973db7b67910bab28129277ffe
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-24 21:27:13 +0300

    Create QuantityFavouriteDetailView layout

---
+++ Touchdown/View/Detail/QuantityFavoutiteDetailView.swift
@@ -0,0 +1,57 @@
+//
+//  QuantityFavoutiteDetailView.swift
+//  Touchdown
+//
+//  Created by Tacettin Küstü on 24.07.2023.
+//
+
+import SwiftUI
+
+struct QuantityFavouriteDetailView: View {
+    
+    @State private var counter: Int = 0
+        
+    var body: some View {
+        HStack(alignment: .center, spacing: 6, content: {
+            Button(action: {
+                if counter > 0 {
+                    counter -= 1
+                }
+            }, label: {
+                Image(systemName: "minus.circle")
+            })
+            
+            Text("\(counter)")
+                .fontWeight(.semibold)
+                .frame(minWidth: 36)
+            
+            Button(action: {
+                if counter < 100 {
+                    counter += 1
+                }
+            }, label: {
+                Image(systemName: "plus.circle")
+            })
+            
+            Spacer()
+            
+            Button(action: {
+            }, label: {
+                Image(systemName: "heart.circle")
+                    .foregroundColor(.pink)
+            })
+        }) //: HSTACK
+        .font(.system(.title, design: .rounded))
+        .foregroundColor(.black)
+        .imageScale(.large)
+    }
+}
+
+struct QuantityFavouriteDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuantityFavouriteDetailView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
+

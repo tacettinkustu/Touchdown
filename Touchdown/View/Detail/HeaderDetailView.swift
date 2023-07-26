6e3346c0a2622b331aa9488cf12777007d862af1
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-26 17:41:35 +0300

    Apply EnvironmentObject into new components

--- Touchdown/View/Detail/HeaderDetailView.swift
+++ Touchdown/View/Detail/HeaderDetailView.swift
@@ -8,24 +8,27 @@
 import SwiftUI
 
 struct HeaderDetailView: View {
-      
-  var body: some View {
-    VStack(alignment: .leading, spacing: 6, content: {
-      Text("Protective Gear")
-      
-      Text(sampleProduct.name)
-        .font(.largeTitle)
-        .fontWeight(.black)
-    }) //: VSTACK
-    .foregroundColor(.white)
-  }
+    
+    @EnvironmentObject var shop: Shop
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 6, content: {
+            Text("Protective Gear")
+            
+            Text(shop.selectedProduct?.name ?? sampleProduct.name)
+                .font(.largeTitle)
+                .fontWeight(.black)
+        }) //: VSTACK
+        .foregroundColor(.white)
+    }
 }
 
 struct HeaderDetailView_Previews: PreviewProvider {
-  static var previews: some View {
-    HeaderDetailView()
-      .previewLayout(.sizeThatFits)
-      .padding()
-      .background(Color.gray)
-  }
+    static var previews: some View {
+        HeaderDetailView()
+            .environmentObject(Shop())
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(Color.gray)
+    }
 }
--- Touchdown/View/Detail/TopPartDetailView.swift
+++ Touchdown/View/Detail/TopPartDetailView.swift
@@ -8,41 +8,45 @@
 import SwiftUI
 
 struct TopPartDetailView: View {
+  
+  @EnvironmentObject var shop: Shop
+  @State private var isAnimating: Bool = false
     
-    @State private var isAnimating: Bool = false
-    
-    var body: some View {
-        HStack(alignment: .center, spacing: 6, content: {
-            VStack(alignment: .leading, spacing: 6, content: {
-                Text("Price")
-                    .fontWeight(.semibold)
-                
-                Text(sampleProduct.formattedPrice)
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                    .scaleEffect(1.35, anchor: .leading)
-            })
-            .offset(y: isAnimating ? -50 : -75)
-            
-            Spacer()
-            
-            Image(sampleProduct.image)
-                .resizable()
-                .scaledToFit()
-                .offset(y: isAnimating ? 0 : -35)
-        }) //: HSTACK
-        .onAppear(perform: {
-            withAnimation(.easeOut(duration: 0.75)) {
-                isAnimating.toggle()
-            }
-        })
-    }
+  var body: some View {
+    HStack(alignment: .center, spacing: 6, content: {
+      VStack(alignment: .leading, spacing: 6, content: {
+        Text("Price")
+          .fontWeight(.semibold)
+        
+        Text(shop.selectedProduct?.formattedPrice ?? sampleProduct.formattedPrice)
+          .font(.largeTitle)
+          .fontWeight(.black)
+          .scaleEffect(1.35, anchor: .leading)
+      })
+      .offset(y: isAnimating ? -50 : -75)
+      
+      Spacer()
+      
+      // PHOTO
+      Image(shop.selectedProduct?.image ?? sampleProduct.image)
+        .resizable()
+        .scaledToFit()
+        .offset(y: isAnimating ? 0 : -35)
+    }) //: HSTACK
+    .onAppear(perform: {
+      withAnimation(.easeOut(duration: 0.75)) {
+        isAnimating.toggle()
+      }
+    })
+  }
 }
 
 struct TopPartDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        TopPartDetailView()
-            .previewLayout(.sizeThatFits)
-            .padding()
-    }
+  static var previews: some View {
+    TopPartDetailView()
+      .environmentObject(Shop())
+      .previewLayout(.sizeThatFits)
+      .padding()
+  }
 }
+

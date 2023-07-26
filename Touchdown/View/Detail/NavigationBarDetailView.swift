8bd95c8788979b0ad0e65b5706e22f323b12b6c2
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-26 17:30:57 +0300

    Add back-button action on productDetails

--- Touchdown/View/Detail/NavigationBarDetailView.swift
+++ Touchdown/View/Detail/NavigationBarDetailView.swift
@@ -8,10 +8,16 @@
 import SwiftUI
 
 struct NavigationBarDetailView: View {
-        
+  
+  @EnvironmentObject var shop: Shop
+      
   var body: some View {
     HStack {
       Button(action: {
+        withAnimation(.easeIn) {
+          shop.selectedProduct = nil
+          shop.showingProduct = false
+        }
       }, label: {
         Image(systemName: "chevron.left")
           .font(.title)
@@ -32,6 +38,7 @@ struct NavigationBarDetailView: View {
 struct NavigationBarDetailView_Previews: PreviewProvider {
   static var previews: some View {
     NavigationBarDetailView()
+      .environmentObject(Shop())
       .previewLayout(.sizeThatFits)
       .padding()
       .background(Color.gray)
